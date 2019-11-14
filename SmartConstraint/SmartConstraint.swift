0f23c9e564da2eeffbc3f6faf910d7ef26a8f860
jonhpol <j.paulo_os@hotmail.com>
2019-11-14 16:34:05 -0300

    Improve implementation

--- SmartConstraint/SmartConstraint.swift
+++ SmartConstraint/SmartConstraint.swift
@@ -10,32 +10,40 @@ import UIKit
 
 public class SmartConstraint {
     /// Item to be anchored, can be a UIView or a UILayoutGuide
-    unowned var item: AnyObject
+    unowned var view: AnyObject
     
-    /// All constraints setted to item
-    var constraints: [NSLayoutConstraint] = []
-    
+    public var constraints: [NSLayoutConstraint] = []
+    public var lastConstraint: NSLayoutConstraint? {
+        didSet { if let constraint = lastConstraint { constraints.append(constraint) } }
+    }
     /// Init with View
     init(view: UIView) {
         view.translatesAutoresizingMaskIntoConstraints = false
-        self.item = view
+        self.view = view
     }
     
     /// Init with Layout Guide
     init(layoutGuide: UILayoutGuide) {
-        self.item = layoutGuide
+        self.view = layoutGuide
     }
     
     /// Activate all constraints setted in constraints array
     static public func activate(_ constraints: SmartConstraint ...) {
         constraints.forEach {
-            if let view = $0.item as? UIView {
+            if let view = $0.view as? UIView {
                 view.translatesAutoresizingMaskIntoConstraints = false
             }
             NSLayoutConstraint.activate($0.constraints)
         }
         
     }
+    
+    /// Deactivate all constraints
+    public func deactivateAll() {
+        NSLayoutConstraint.deactivate(constraints)
+        constraints.removeAll()
+    }
+
 }
 
 public extension UIView {
@@ -48,72 +56,149 @@ public extension UIView {
 extension SmartConstraint {
     
     @discardableResult
-    public func top(_ anchor: NSLayoutYAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.topAnchor, toAnchor: anchor, relation: relation, padding: padding)
+    public func top(_ anchor: NSLayoutYAxisAnchor,
+                    padding: CGFloat = 0,
+                    relation: NSLayoutConstraint.Relation = .equal,
+                    priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.topAnchor, toAnchor: anchor, relation: relation, padding: padding, priority: priority)
         return self
     }
     
     @discardableResult
-    public func bottom(_ anchor: NSLayoutYAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.bottomAnchor, toAnchor: anchor, relation: relation, padding: -padding)
+    public func bottom(_ anchor: NSLayoutYAxisAnchor,
+                       padding: CGFloat = 0,
+                       relation: NSLayoutConstraint.Relation = .equal,
+                       priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.bottomAnchor, toAnchor: anchor, relation: relation, padding: -padding, priority: priority)
         return self
     }
     
     @discardableResult
-    public func left(_ anchor: NSLayoutXAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.leftAnchor, toAnchor: anchor, relation: relation, padding: padding)
+    public func left(_ anchor: NSLayoutXAxisAnchor,
+                     padding: CGFloat = 0,
+                     relation: NSLayoutConstraint.Relation = .equal,
+                     priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.leftAnchor, toAnchor: anchor, relation: relation, padding: padding, priority: priority)
         return self
     }
     
     @discardableResult
-    public func right(_ anchor: NSLayoutXAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.rightAnchor, toAnchor: anchor, relation: relation, padding: -padding)
+    public func right(_ anchor: NSLayoutXAxisAnchor,
+                      padding: CGFloat = 0,
+                      relation: NSLayoutConstraint.Relation = .equal,
+                      priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.rightAnchor, toAnchor: anchor, relation: relation, padding: -padding, priority: priority)
         return self
     }
     
     @discardableResult
-    public func width(_ anchor: NSLayoutDimension, constant: CGFloat = 0, multiplier: CGFloat = 1, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutDimension(item.widthAnchor, toDimension: anchor, relation: relation, constant: constant, multiplier: multiplier)
+    public func width(_ anchor: NSLayoutDimension,
+                      constant: CGFloat = 0,
+                      multiplier: CGFloat = 1,
+                      relation: NSLayoutConstraint.Relation = .equal,
+                      priority: Float = 1000) -> SmartConstraint {
+        layoutDimension(view.widthAnchor, toDimension: anchor, relation: relation, constant: constant, multiplier: multiplier, priority: priority)
         return self
     }
     
     @discardableResult
-    public func width(constant: CGFloat, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutDimension(item.widthAnchor, relation: relation, constant: constant)
+    public func width(constant: CGFloat,
+                      relation: NSLayoutConstraint.Relation = .equal,
+                      priority: Float = 1000) -> SmartConstraint {
+        layoutDimension(view.widthAnchor, relation: relation, constant: constant, priority: priority)
         return self
     }
     
     @discardableResult
-    public func height(_ anchor: NSLayoutDimension, constant: CGFloat = 0, multiplier: CGFloat = 1, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutDimension(item.heightAnchor, toDimension: anchor, relation: relation, constant: constant, multiplier: multiplier)
+    public func height(_ anchor: NSLayoutDimension,
+                       constant: CGFloat = 0,
+                       multiplier: CGFloat = 1,
+                       relation: NSLayoutConstraint.Relation = .equal,
+                       priority: Float = 1000) -> SmartConstraint {
+        layoutDimension(view.heightAnchor, toDimension: anchor, relation: relation, constant: constant, multiplier: multiplier, priority: priority)
         return self
     }
     
     @discardableResult
-    public func height(constant: CGFloat, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutDimension(item.heightAnchor, relation: relation, constant: constant)
+    public func height(constant: CGFloat,
+                       relation: NSLayoutConstraint.Relation = .equal,
+                       priority: Float = 1000) -> SmartConstraint {
+        layoutDimension(view.heightAnchor, relation: relation, constant: constant, priority: priority)
         return self
     }
     
     @discardableResult
-    public func centerX(_ anchor: NSLayoutXAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.centerXAnchor, toAnchor: anchor, relation: relation, padding: padding)
+    public func centerX(_ anchor: NSLayoutXAxisAnchor,
+                        padding: CGFloat = 0,
+                        relation: NSLayoutConstraint.Relation = .equal,
+                        priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.centerXAnchor, toAnchor: anchor, relation: relation, padding: padding, priority: priority)
         return self
     }
     
     @discardableResult
-    public func centerY(_ anchor: NSLayoutYAxisAnchor, padding: CGFloat = 0, relation: NSLayoutConstraint.Relation = .equal) -> SmartConstraint {
-        layoutAnchor(item.centerYAnchor, toAnchor: anchor, relation: relation, padding: padding)
+    public func centerY(_ anchor: NSLayoutYAxisAnchor,
+                        padding: CGFloat = 0,
+                        relation: NSLayoutConstraint.Relation = .equal, priority: Float = 1000) -> SmartConstraint {
+        layoutAnchor(view.centerYAnchor, toAnchor: anchor, relation: relation, padding: padding, priority: priority)
         return self
     }
     
-    public func attatch() -> SmartConstraint {
+    @discardableResult
+    public func attatch(to view: UIView, paddings: [Paddings]? = nil) -> SmartConstraint {
+        var anchors: [CGFloat] = [0, 0, 0, 0]
+        paddings?.forEach({ (padding) in
+            switch padding {
+            case .top(let anchor):
+                anchors[0] = anchor
+            case .right(let anchor):
+                anchors[1] = anchor
+            case .bottom(let anchor):
+                anchors[2] = anchor
+            case .left(let anchor):
+                anchors[3] = anchor
+            }
+            
+        })
         
+        top(view.topAnchor, padding: anchors[0])
+        right(view.rightAnchor, padding: anchors[1])
+        bottom(view.bottomAnchor, padding: anchors[2])
+        left(view.leftAnchor, padding: anchors[3])
         return self
     }
     
-    func layoutAnchor<T: AnyObject>(_ anchor: NSLayoutAnchor<T>, toAnchor: NSLayoutAnchor<T>, relation: NSLayoutConstraint.Relation, padding: CGFloat) {
-        var constraint: NSLayoutConstraint?
+    @discardableResult
+    public func attatch(to layoutGuide: UILayoutGuide, paddings: [Paddings]? = nil) -> SmartConstraint {
+        var anchors: [CGFloat] = [0, 0, 0, 0]
+        
+        paddings?.forEach({ (padding) in
+            switch padding {
+            case .top(let anchor):
+                anchors[0] = anchor
+            case .right(let anchor):
+                anchors[1] = anchor
+            case .bottom(let anchor):
+                anchors[2] = anchor
+            case .left(let anchor):
+                anchors[3] = anchor
+            }
+            
+        })
+        
+        top(layoutGuide.topAnchor, padding: anchors[0])
+        right(layoutGuide.rightAnchor, padding: anchors[1])
+        bottom(layoutGuide.bottomAnchor, padding: anchors[2])
+        left(layoutGuide.leftAnchor, padding: anchors[3])
+        return self
+    }
+    
+    func layoutAnchor<T: AnyObject>(_ anchor: NSLayoutAnchor<T>,
+                                    toAnchor: NSLayoutAnchor<T>,
+                                    relation: NSLayoutConstraint.Relation,
+                                    padding: CGFloat,
+                                    priority: Float) {
+        var constraint: NSLayoutConstraint!
         
         switch relation {
         case .equal:
@@ -125,15 +210,19 @@ extension SmartConstraint {
         default:
             fatalError()
         }
-        
-        if let const = constraint {
-            const.isActive = true
-            constraints.append(const)
-        }
+        constraint.priority = UILayoutPriority(priority)
+        constraint.isActive = true
+        lastConstraint = constraint
     }
     
-    func layoutDimension(_ dimension: NSLayoutDimension, toDimension: NSLayoutDimension? = nil, relation: NSLayoutConstraint.Relation, constant: CGFloat, multiplier: CGFloat = 1) {
-        var constraint: NSLayoutConstraint?
+    func layoutDimension(_ dimension: NSLayoutDimension,
+                         toDimension: NSLayoutDimension? = nil,
+                         relation: NSLayoutConstraint.Relation,
+                         constant: CGFloat,
+                         multiplier: CGFloat = 1,
+                         priority: Float = 1000) {
+        
+        var constraint: NSLayoutConstraint!
         
         switch relation {
         case .equal:
@@ -157,10 +246,15 @@ extension SmartConstraint {
         default:
             fatalError()
         }
-        
-        if let const = constraint {
-            const.isActive = true
-            constraints.append(const)
-        }
+        constraint.priority = UILayoutPriority(priority)
+        constraint.isActive = true
+        lastConstraint = constraint
     }
+}
+
+public enum Paddings: Equatable {
+    case top(CGFloat)
+    case right(CGFloat)
+    case bottom(CGFloat)
+    case left(CGFloat)
 }
