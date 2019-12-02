1fbd10cb2162b903df14b2d3f1402bf2a6ec6e87
jonhpol <j.paulo_os@hotmail.com>
2019-12-02 16:21:13 -0300

    Add platforms versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SmartConstraint",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v10), .tvOS(.v11),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
